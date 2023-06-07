c2e9948e2b8c102036e5b1d2a132d0e31b0c00e9
Matt Crump <crumpmj@gmail.com>
2023-06-07 12:25:48 -0400

    function documentation

--- R/addins.R
+++ R/addins.R
@@ -1,4 +1,20 @@
 
+#' line_editor
+#'
+#' This is an RStudio addin function that should become available after the package is installed.
+#'
+#' Highlight text in an editor window while using Rstudio, then select the addin from the drop-down menu.
+#'
+#' This function sends the selected text to a gpt-3.5-turbo chat model. The system prompt is:
+#'
+#' You are an editorial writing assistant. Your task is to edit the content you receive for improved clarity, flow, and reduced word count.
+#'
+#' The model receives the highlighted text, generates a response following the prompt, and returns to the new suggested text to the console.
+#'
+#' @return
+#' @export
+#'
+#' @examples
 line_editor <- function(){
 
   # get selected text
