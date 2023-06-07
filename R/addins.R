86d93390ba8500a41ccf598ee144b0c79c45fda2
Matt Crump <crumpmj@gmail.com>
2023-06-07 13:12:15 -0400

    add points to prose

--- R/addins.R
+++ R/addins.R
@@ -1,5 +1,5 @@
 
-#' line_editor
+#' Get suggested line edits
 #'
 #' This is an RStudio addin function that should become available after the package is installed.
 #'
@@ -11,10 +11,9 @@
 #'
 #' The model receives the highlighted text, generates a response following the prompt, and returns to the new suggested text to the console.
 #'
-#' @return
+#' @return string to console
 #' @export
 #'
-#' @examples
 line_editor <- function(){
 
 
@@ -30,6 +29,50 @@ line_editor <- function(){
       list(
         "role" = "system",
         "content" = "You are an editorial writing assistant. Your task is to edit the content you receive for improved clarity, flow, and reduced word count."
+      ),
+      list(
+        "role" = "user",
+        "content" = selected_text$value
+      )
+    )
+  )
+
+  # print the results to the console
+  print(gpt$choices$message.content)
+}
+
+
+
+#' Convert point form to longform prose
+#'
+#' This is an RStudio addin function that should become available after the package is installed.
+#'
+#' Highlight point form text in an editor window while using Rstudio, then select the addin from the drop-down menu.
+#'
+#' This function sends the selected text to a gpt-3.5-turbo chat model. The system prompt is:
+#'
+#' You are a writing assistant. Your task is to take point form notes and convert them into longform prose. Please write in a straightforward and clear manner. The user prompt will contain point form notes.
+#'
+#' The model receives the highlighted text, generates a response following the prompt, and returns to the new suggested text to the console.
+#'
+#' @return string to console
+#' @export
+#'
+points_to_prose <- function(){
+
+
+  # get selected text
+  selected_text <- rstudioapi::selectionGet()
+
+  message("Submitting query...")
+
+  # run the api call to openai
+  gpt <- openai::create_chat_completion(
+    model = "gpt-3.5-turbo",
+    messages = list(
+      list(
+        "role" = "system",
+        "content" = "You are a writing assistant. Your task is to take point form notes and convert them into longform prose. Please write in a straightforward and clear manner. The user prompt will contain point form notes."
       ),
       list(
         "role" = "user",
