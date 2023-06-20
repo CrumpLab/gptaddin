b52cd2e4efb683d99b2a003479588affedf67ffd
Matt Crump <crumpmj@gmail.com>
2023-06-20 09:04:08 -0400

    add continuous chat with memory

--- R/addins.R
+++ R/addins.R
@@ -41,6 +41,48 @@ line_editor <- function(){
   print(cat(gpt$choices$message.content))
 }
 
+#' Get suggested line edits with markdown flagged changes
+#'
+#' This is an RStudio addin function that should become available after the package is installed.
+#'
+#' Highlight text in an editor window while using Rstudio, then select the addin from the drop-down menu.
+#'
+#' This function sends the selected text to a gpt-3.5-turbo chat model. The system prompt is:
+#'
+#' You are an editorial writing assistant. Your task is to edit the content you receive for improved clarity, flow, and reduced word count. You should respect the original style of the input text and use an active voice. Please return the edited text using markdown syntax to highlight all the changed wording. Specifically, put bold all of the changed words or phrases that are different from the original text.
+#'
+#' The model receives the highlighted text, generates a response following the prompt, and returns the new suggested text to the console.
+#'
+#' @return string to console
+#' @export
+#'
+line_editor_markdown <- function(){
+
+
+  # get selected text
+  selected_text <- rstudioapi::selectionGet()
+
+  message("Submitting query...")
+
+  # run the api call to openai
+  gpt <- openai::create_chat_completion(
+    model = "gpt-3.5-turbo",
+    messages = list(
+      list(
+        "role" = "system",
+        "content" = "You are an editorial writing assistant. Your task is to edit the content you receive for improved clarity, flow, and reduced word count. You should respect the original style of the input text and use an active voice. Please return the edited text using markdown syntax to bold any of the suggested changes."
+      ),
+      list(
+        "role" = "user",
+        "content" = selected_text$value
+      )
+    )
+  )
+
+  # print the results to the console
+  print(cat(gpt$choices$message.content))
+}
+
 
 
 #' Convert point form to longform prose
@@ -280,6 +322,88 @@ chat_to_doc <- function(){
                          id = active_context$id)
 }
 
+#' Start a continuous chat with message history
+#'
+#' Run this in the console
+#'
+#' Very basic right now, no error checking. Chat history is preserved in my_chat which is returned to the global environment. Running this function initializes my_chat, and deletes anything that was in there.
+#'
+#' @return list called my_chat to the global environment
+#'
+#' @export
+start_chat <- function(){
 
+  # make list to save results
+  my_chat <<- list()
+
+  # initial prompt
+  my_chat$message <<- list(
+    list("role" = "system",
+         "content" = "You are a helpful assistant.")
+  )
+
+  # send prompt
+  gpt <- openai::create_chat_completion(
+    model = "gpt-3.5-turbo",
+    messages = my_chat$message
+  )
+
+  #store results
+  my_chat$history[[1]] <<- gpt
+  my_chat$message[[length(my_chat$message)+1]] <<- list(
+    "role" = gpt$choices$message.role,
+    "content" = gpt$choices$message.content
+  )
+
+  # print response to console
+  print(cat(gpt$choices$message.content))
+
+}
+
+#' Continue a chat with message history
+#'
+#' To be used in combination with start_chat
+#'
+#' Select any text in the editor then run this addin.
+#'
+#' The selected text is combined with the existing message history and sent as a prompt to OpenAI.
+#'
+#' @return string to the editor window
+#'
+#' @export
+continue_chat <- function(){
+
+  # get document id and selection range
+  active_context <- rstudioapi::getActiveDocumentContext()
+  #print(active_context)
+
+  # get selected text
+  selected_text <- rstudioapi::selectionGet()
+
+  my_chat$message[[length(my_chat$message)+1]] <<- list(
+    "role" = "user",
+    "content" = selected_text$value
+  )
+
+  # send prompt
+  gpt <- openai::create_chat_completion(
+    model = "gpt-3.5-turbo",
+    messages = my_chat$message
+  )
+
+  #store results
+  my_chat$history[[length(my_chat$history)+1]] <<- gpt
+  my_chat$message[[length(my_chat$message)+1]] <<- list(
+    "role" = gpt$choices$message.role,
+    "content" = gpt$choices$message.content
+  )
+
+  new_text <- gpt$choices$message.content
+  insert_text <- paste(selected_text,"\n", new_text, sep="\n")
+
+  rstudioapi::insertText(location = active_context$selection[[1]]$range,
+                         text = insert_text,
+                         id = active_context$id)
+}
 
 
