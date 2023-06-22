4836536b61c6d281d63e7549c5ac230a7434a186
Matt Crump <crumpmj@gmail.com>
2023-06-22 15:26:39 -0400

    add shiny app

---
+++ R/grammar_shiny.R
@@ -0,0 +1,153 @@
+check_grammar_gpt <- function(text_to_edit,user_choice){
+
+  # get selected text
+  selected_text <- text_to_edit
+
+  if( user_choice == "spelling_grammar"){
+    system_content <- "You are an editorial writing assistant. Edit the text for spelling and grammar. Don't change the meaning or the words. If the edited sentence is different from the original, then return the results in HTML and highlight any changed words in red. If the sentence does not need to be changed, then return OK."
+  }
+
+  if( user_choice == "word_count"){
+    system_content <- "You are an editorial writing assistant. Your task is to edit the content you receive for reduced word count"
+  }
+
+  if( user_choice == "clarity"){
+    system_content <- "You are an editorial writing assistant. Your task is to edit the content you receive for clarity"
+  }
+
+  if( user_choice == 4){
+    system_content <- readline(prompt = "Enter your prompt")
+  }
+
+  gpt <- openai::create_completion(
+    model = "text-davinci-003",
+    prompt = paste(system_content,"\n",selected_text),
+    max_tokens = 2024,
+
+  )
+
+  #print(gpt) error-checking
+
+  return(gpt$choices$text)
+}
+
+# These functions were inspired by, and rewritten from https://github.com/jasdumas/gramr
+
+#--------------------------------
+# Functions for the Shiny app
+
+#' Parse RMarkdown documents into sentences or paragraphs
+#' @param file a character vector of the file or path
+#' @examples
+#' # don't run
+#' # parse_rmd()
+#' @export
+parse_rmd <- function(file,token_level="sentences"){
+
+  # Read in the R Markdown file
+  rmd_file <- readLines(file)
+
+  if(token_level == "sentences"){
+    # Parse the Rmd file into individual sentences
+    sentences <- unlist(tokenizers::tokenize_sentences(rmd_file))
+  }
+
+  if(token_level == "paragraphs"){
+    # Parse the Rmd file into individual sentences
+    sentences <- unlist(tokenizers::tokenize_paragraphs(rmd_file))
+  }
+
+  sentences
+
+}
+
+
+#' Start grammar checker shiny application
+#' Start the grammar checker.
+#' @param path the intended filepath
+#' @param choose_token_level string "sentences" or "paragraphs", defaults to sentences
+#' @return a shiny app is launched
+#' @import shiny
+#' @examples
+#' # don't run during tests
+#' # gptaddin::run_grammar_checker("example.rmd")
+#' @export
+run_grammar_checker <- function(path, choose_token_level = "sentences"){
+  sentences <- parse_rmd(path, choose_token_level)
+  counter <- 1
+
+  ui <- fluidPage(
+    # Application title
+    titlePanel(
+      "GPT Grammar checker"
+    ),
+    sidebarLayout(
+      sidebarPanel(
+        selectInput(inputId = 'options',
+                           label = "Choose prompt",
+                           choices = list("Basic Spelling and grammar" = "spelling_grammar",
+                                            "Reduced word count" = "word_count",
+                                            "Improved clarity" = "clarity"),
+                            multiple = FALSE
+        ),
+        actionButton("nextS", "Next"),
+        actionButton("submit","Submit"),
+        actionButton("backS", "Back"),
+        actionButton("exit", "Finish")
+      ),
+      mainPanel(
+        textAreaInput(inputId  = 'text',
+                      label  = 'Text to Check',
+                      value  = sentences[1],
+                      height = 150,
+                      width = 500,
+                      resize = "both"),
+        h3("Suggestion"),
+        htmlOutput(outputId = "text")
+      )
+    )
+  )
+
+  server <- function(input, output, session) {
+
+    # go to next sentence
+    observeEvent(input$nextS, {
+      if( counter < length(sentences)){
+        counter <<- counter + 1
+        updateTextInput(session, "text", value = sentences[counter])
+      }
+    })
+
+    # go to previous sentence
+    observeEvent(input$backS, {
+      if( counter > 1){
+        counter <<- counter - 1
+        updateTextInput(session, "text", value = sentences[counter])
+      }
+    })
+
+    # submit sentence for checking to OpenAI API
+    observeEvent(input$submit, {
+      #print(input$options)
+      output$text <- renderText({
+        "Submitting text..."
+      })
+      suggestion <- check_grammar_gpt(sentences[counter],input$options)
+      output$text <- renderUI({
+        HTML(suggestion)
+      })
+    })
+
+    # exit the app
+    observeEvent(input$exit, {
+      #text_df[ text_df$line_num == check_df$line_num[counter], "text"] <- input$text
+      #writeLines(text_df$text, path)
+      stopApp()
+    })
+  }
+
+  # Run the application
+  shinyApp(ui = ui, server = server)
+}
+
+
